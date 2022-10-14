2cc6c6be668d3ad131a6a5bcea7612bcbf7bf409
farhanyousaf786 <farhanyousaf1996@gmail.com>
2022-10-14 15:08:17 -0400

    remove everything

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -6,57 +6,14 @@ import './LandingPage.css';
 function LandingPage() {
   
   return (
-    <div className="background">
 
-<aside class="profile-card">
-  <header>
-    <a target="_blank" href="#">
-      <img src="http://lorempixel.com/150/150/people/" class="hoverZoomLink"/>
-    </a>
+<>
 
-    <h1>
-            Farhan Yousaf
-          </h1>
+<div class="container">
 
-    <h2>
-            Full Stack Developer
-          </h2>
+</div>
+</>
 
-  </header>
-
-  <div class="profile-bio">
-
-    <p>
-      It takes monumental improvement for us to change how we live our lives. Design is the way we access that improvement.
-    </p>
-
-  </div>
-
-  <ul class="profile-social-links">
-    <li>
-      <a target="_blank" href="https://www.facebook.com/creativedonut">
-        <i class="fa fa-facebook"></i>
-      </a>
-    </li>
-    <li>
-      <a target="_blank" href="https://twitter.com/dropyourbass">
-        <i class="fa fa-twitter"></i>
-      </a>
-    </li>
-    <li>
-      <a target="_blank" href="https://github.com/vipulsaxena">
-        <i class="fa fa-github"></i>
-      </a>
-    </li>
-    <li>
-      <a target="_blank" href="https://www.behance.net/vipulsaxena">
-       
-        <i class="fa fa-behance"></i>
-      </a>
-    </li>
-  </ul>
-</aside>
- </div>
   );
 }
 

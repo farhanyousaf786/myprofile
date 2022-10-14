0347d9ba7d00a6a332994aa45436a63ccc95019e
farhanyousaf786 <farhanyousaf1996@gmail.com>
2022-10-14 18:48:58 -0400

    Deploy

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -12,36 +12,26 @@ function LandingPage() {
   return (
 
 
-<div className="main">
-
-  <div className = "left-container" >
-          <div className="profile-avatar">
-      <img className="profile-img" src="https://imgur.com/ggbj9e9.png" alt=""/>
-      </div>
-      
-  </div>
+   <div className="main">
+   <div className = "left-container" >
+   <div className="profile-avatar">
+   <img className="profile-img" src="https://imgur.com/ggbj9e9.png" alt=""/>
+   </div>
+   </div>
   
 
-
-
-  <div className="right-container">
   
-
-  <center>
-  <div className='lottie1'>  <Lottie animationData={animationData} loop={true}/>
-</div>
-    </center>
-  <div className="main-caption">
-  
-  <h1><span>Hi! I am Farhan Yousaf.</span></h1>
-  <h1><span>I am full stack software developer.</span></h1>
-  <h1><span>Developed in React & Flutter full.</span></h1>
-   
-  </div>
-
-  </div>
-
-</div>
+   <div className="right-container">
+   <center>
+   <div className='lottie1'>  <Lottie animationData={animationData} loop={true}/></div>
+   </center>
+   <div className="main-caption">
+   <h1><span>Hi! I am Farhan Yousaf.</span></h1>
+   <h1><span>I am full stack software developer.</span></h1>
+   <h1><span>Developed in React & Flutter full.</span></h1>
+   </div>
+   </div>
+   </div>
 
   );
 }

76a0ed585bda1332fff1ea1a64420e08fe0c57cc
farhanyousaf786 <farhanyousaf1996@gmail.com>
2022-10-14 12:36:59 -0400

    added landing page and added landing css

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -1,24 +1,62 @@
-import React from "react";
+import React, { useState } from 'react';
 import './LandingPage.css';
-// import { useEffect } from 'react';
+
 
 
 function LandingPage() {
   
-  // useEffect(() => {
-  //   const script = document.createElement('script');
-  //   script.src = "./LandingPage.js";
-  //   script.async = true;
-  //   document.body.appendChild(script);
-  // return () => {
-  //     document.body.removeChild(script);
-  //   }
-  // }, []);
-  
   return (
     <div className="background">
-    <h1>This is my info page </h1>
-    </div>
+
+<aside class="profile-card">
+  <header>
+    <a target="_blank" href="#">
+      <img src="http://lorempixel.com/150/150/people/" class="hoverZoomLink"/>
+    </a>
+
+    <h1>
+            Farhan Yousaf
+          </h1>
+
+    <h2>
+            Full Stack Developer
+          </h2>
+
+  </header>
+
+  <div class="profile-bio">
+
+    <p>
+      It takes monumental improvement for us to change how we live our lives. Design is the way we access that improvement.
+    </p>
+
+  </div>
+
+  <ul class="profile-social-links">
+    <li>
+      <a target="_blank" href="https://www.facebook.com/creativedonut">
+        <i class="fa fa-facebook"></i>
+      </a>
+    </li>
+    <li>
+      <a target="_blank" href="https://twitter.com/dropyourbass">
+        <i class="fa fa-twitter"></i>
+      </a>
+    </li>
+    <li>
+      <a target="_blank" href="https://github.com/vipulsaxena">
+        <i class="fa fa-github"></i>
+      </a>
+    </li>
+    <li>
+      <a target="_blank" href="https://www.behance.net/vipulsaxena">
+       
+        <i class="fa fa-behance"></i>
+      </a>
+    </li>
+  </ul>
+</aside>
+ </div>
   );
 }
 

ea57c9529a3f2c5072cecc91315b7d1a60f6097f
farhanyousaf786 <farhanyousaf1996@gmail.com>
2022-10-15 18:24:05 -0400

    added card for projects

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -8,21 +8,19 @@ function Card() {
     return(
 
 
-
-
 <div class="wrapper">
-  <h1>Parallax Flipping Cards</h1>
+  <h1>Projects </h1>
   <div class="cols">
 			<div class="col" ontouchstart="this.classList.toggle('hover');">
 				<div class="container">
 					<div class="front" >
 						<div class="inner">
-							<p>Diligord</p>
+							<p>Switch APP</p>
 						</div>
 					</div>
 					<div class="back">
 						<div class="inner">
-						  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias cum repellat velit quae suscipit c.</p>
+						  <p>Info will be here..</p>
 						</div>
 					</div>
 				</div>
--- src/Pages/MyProjects/MyProjects.jsx
+++ src/Pages/MyProjects/MyProjects.jsx
@@ -1,16 +1,19 @@
 
 import React from 'react';
-import Card from '../../Components/Card/Card';
+import Cards from '../../Components/Card/Card';
 // import { useMediaQuery } from 'react-responsive'
 import './MyProjects.css';
 
 
 
 function MyProjects() {
+    
+
+
 
     return(
 
-          <Card/>
+          <Cards/>
 
     );
 

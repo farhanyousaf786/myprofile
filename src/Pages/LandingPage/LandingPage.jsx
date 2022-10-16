eb4958274c657f55c94a2f5d2e8bfb63684989c3
farhanyousaf786 <farhanyousaf1996@gmail.com>
2022-10-16 02:00:11 -0400

    added responsive element css

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import LeftSide from '../LeftSide/LeftSide';
 import RightSide from '../RightSide/RightSide';
 import './LandingPage.css';
@@ -6,29 +5,53 @@ import { useMediaQuery } from 'react-responsive'
 import TabBar from '../../Components/Header/Header';
 import MyProjects from '../MyProjects/MyProjects';
 import {useRef} from 'react';
+import React, { useState, useEffect } from "react";
+
 
 
 
 
 function LandingPage() {
 
+  const isDesktopOrLaptop = useMediaQuery({query: '(min-width: 1224px)' })
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
+
+  let hideHeader = "false";
   const ref = useRef(null);
 
   const handleClick = () => {
     ref.current?.scrollIntoView({behavior: 'smooth'});
+     
   };
 
+ 
+  
+  let oldScrollY = 0;
 
-  const isDesktopOrLaptop = useMediaQuery({query: '(min-width: 1224px)' })
-  const isBigScreen = useMediaQuery({ query: '(min-width: 1824px)' })
-  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
-  const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
-  const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' })
+const [direction, setDirection] = useState('up');
+
+const controlDirection = () => {
+    if(window.scrollY > oldScrollY) {
+        setDirection('down');
+    } else {
+        setDirection('up');
+    }
+    oldScrollY = window.scrollY;
+}
+
+useEffect(() => {
+    window.addEventListener('scroll', controlDirection);
+    return () => {
+        window.removeEventListener('scroll', controlDirection);
+    };
+},[]);
 
   return (
 
     
-    <div>{isTabletOrMobile && <TabBar/>}
+    <div>{direction === "up" ?  isTabletOrMobile && <TabBar/> : null}
+
+
     <div className="main">
     {isDesktopOrLaptop && <LeftSide/>}
     {isDesktopOrLaptop && <RightSide
@@ -36,7 +59,11 @@ function LandingPage() {
           handleClick={handleClick}
           
           />}
-    {isTabletOrMobile && <div className='tab-padding'> <RightSide/> </div>}
+    {isTabletOrMobile && <div className='tab-padding'> <RightSide
+    
+    handleClick={handleClick}
+
+    /> </div>}
     </div>
     <div ref={ref}><MyProjects/></div>
 

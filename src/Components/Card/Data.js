ce2ce3a8a570a6c43fc8e865da15dfde2f5f9a63
farhanyousaf786 <farhanyousaf1996@gmail.com>
2022-10-16 01:26:21 -0400

    added projects

--- src/Components/Card/Data.js
+++ src/Components/Card/Data.js
@@ -12,8 +12,7 @@ export default [
     {
       platform: 'React',
 
-      img:
-        "https://images.unsplash.com/photo-1498712964741-5d33ab9e5017?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=600",
+      img: "https://imgur.com/MKsKnrA.png",
       title: "NFT Market",
       detail:"This web app is to represent your nfts. You can upload you nfts in gif or in simple image. Tect used init are HTML, CSS + CSS Table, MongoDB - Express, React (Front-End), MongoDB atlas for live server, Heroku - for deploy application.",
         gitRepo: "https://github.com/farhanyousaf786/nft-webapp"
@@ -22,8 +21,7 @@ export default [
 
       platform: 'Node.js',
 
-      img:
-        "https://images.unsplash.com/photo-1498712964741-5d33ab9e5017?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=600",
+      img: "https://imgur.com/8RXbyW2.png",
       title: "MEME App",
       detail:"A Meme Web App, that uses to upload your fav. meme. Tech used init are Python, Django, PostgreSQL CSS, HTML, GitHub, Amazon Web Servies, S3 Buckets, Heroku.",
         gitRepo: "https://github.com/farhanyousaf786/memeapp-1"
@@ -32,8 +30,7 @@ export default [
 
       platform: 'Java-Script',
 
-      img:
-        "https://images.unsplash.com/photo-1498712964741-5d33ab9e5017?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=600",
+      img: "https://imgur.com/nuyd6kX.png",
       title: "Connect 4",
       detail: "Connect Four is a two-player strategy game played on a 7-column by 7-row board. Tect used init are HTML, Script, CSS + CSS Table, PixelLab (To create Logos), GitHub for version control.",
         gitRepo: "https://github.com/farhanyousaf786/Connect-Four"

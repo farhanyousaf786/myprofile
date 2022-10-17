5ca8f2dd2e37a04d4a6b002df335b4a8106c5a93
farhanyousaf786 <farhanyousaf1996@gmail.com>
2022-10-17 15:40:50 -0400

    added responsive element css

--- src/Components/Card/Data.js
+++ src/Components/Card/Data.js
@@ -5,7 +5,7 @@ export default [
       img: "https://imgur.com/4YZWOcS.png",
       title: "Switch App",
       detail:
-        "Switch is an social media app. Its also my fav. project, developed in flutter. Moreover, Firebase, Dart and Digital Ocean used init.",
+        "Developed in flutter. Moreover, Firebase, Dart and Digital Ocean used init.",
       gitRepo: "https://github.com/farhanyousaf786/switch-app"
     },
 
@@ -14,7 +14,7 @@ export default [
 
       img: "https://imgur.com/MKsKnrA.png",
       title: "NFT Market",
-      detail:"This web app is to represent your nfts. You can upload you nfts in gif or in simple image. Tect used init are HTML, CSS + CSS Table, MongoDB - Express, React (Front-End), MongoDB atlas for live server, Heroku - for deploy application.",
+      detail:"HTML, CSS + CSS Table, MongoDB - Express, React, MongoDB atlas for live server, Heroku.",
         gitRepo: "https://github.com/farhanyousaf786/nft-webapp"
     },
     {
@@ -23,7 +23,7 @@ export default [
 
       img: "https://imgur.com/8RXbyW2.png",
       title: "MEME App",
-      detail:"A Meme Web App, that uses to upload your fav. meme. Tech used init are Python, Django, PostgreSQL CSS, HTML, GitHub, Amazon Web Servies, S3 Buckets, Heroku.",
+      detail:"Tech used are Python, Django, PostgreSQL CSS, HTML, GitHub, Amazon Web Servies, S3 Buckets, Heroku.",
         gitRepo: "https://github.com/farhanyousaf786/memeapp-1"
     },
     {
@@ -32,7 +32,7 @@ export default [
 
       img: "https://imgur.com/nuyd6kX.png",
       title: "Connect 4",
-      detail: "Connect Four is a two-player strategy game played on a 7-column by 7-row board. Tect used init are HTML, Script, CSS + CSS Table, PixelLab (To create Logos), GitHub for version control.",
+      detail: "Tect used are HTML, Script, CSS + CSS Table, PixelLab (To create Logos), GitHub for version control.",
         gitRepo: "https://github.com/farhanyousaf786/Connect-Four"
     },
   

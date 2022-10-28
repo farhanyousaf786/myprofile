e17e51688cb451cee48de7f176d45a0f075424c8
farhanyousaf786 <farhanyousaf1996@gmail.com>
2022-10-28 15:47:38 -0400

    edited css to get responsive ui

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -9,15 +9,19 @@ import Lottie from "lottie-react";
 function TabBar() {
 
     return(
+        
         <div className='header'>
-        {/* <img className="header-img" src="https://imgur.com/EExH9op.png" alt=""/> */}
-
+        
         <a className='header-img' href='https://github.com/farhanyousaf786/switch-app' target="_blank" rel="noreferrer">
         <div className='fav-project'>
-        {/* <h1 className='heading' >Favourite</h1>     */}
         <div className='lottie2'>  <Lottie animationData={animationData} loop={true}/></div>
         </div>
         </a>
+        
+
+
+       
+        
     
         </div>
     )
